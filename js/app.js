f0ebafced00971c2efa1b7d38c15162c42d0149e
Jeff_Gore <JefGore@gmail.com>
2021-10-16 11:34:59 -0400

    HTML in place

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*----------------- Constants -----------------*/
+
+/*------------- Variables (state) -------------*/
+
+/*--------- Cached Element References ---------*/
+
+/*-------------- Event Listeners --------------*/
+
+/*----------------- Functions -----------------*/
+
+
+// 1. A connect four board, a reset button, and the number of moves remaining for each side is visible.
+// 2. Game will display which color’s turn it currently is.
+// 3. A click event will be tied to the board.
+// 4. Clicking on any circle in the board will check current board state. 
+// 5. A colored chip of the active turn will appear in lowest available spot of the column clicked
+// 6. The code will check the board to see if a winning condition is has been met.
+// 7. If a winning combination has not been met, steps 2 -6 will repeat
+// 8. If a winning condition has been met, a winning message will display, and the game will end.
+// 9. Clicking the reset button will return the game to the state present at condition 
+
+
