59b52f40b0fa6d394a3dc182a8cfabecd3fe0a52
Jeff_Gore <JefGore@gmail.com>
2021-10-20 15:02:42 -0400

    find winner and additional styling

--- js/app.js
+++ js/app.js
@@ -34,6 +34,77 @@ let boardState = [
   null, null, null, null, null, null
 ]
 
+let winningCombos = [
+  [0, 1, 2, 3],
+[41, 40, 39, 38],
+[7, 8, 9, 10],
+[34, 33, 32, 31],
+[14, 15, 16, 17],
+[27, 26, 25, 24],
+[21, 22, 23, 24],
+[20, 19, 18, 17],
+[28, 29, 30, 31],
+[13, 12, 11, 10],
+[35, 36, 37, 38],
+[6, 5, 4, 3],
+[0, 7, 14, 21],
+[41, 34, 27, 20],
+[1, 8, 15, 22],
+[40, 33, 26, 19],
+[2, 9, 16, 23],
+[39, 32, 25, 18],
+[3, 10, 17, 24],
+[38, 31, 24, 17],
+[4, 11, 18, 25],
+[37, 30, 23, 16],
+[5, 12, 19, 26],
+[36, 29, 22, 15],
+[6, 13, 20, 27],
+[35, 28, 21, 14],
+[0, 8, 16, 24],
+[41, 33, 25, 17],
+[7, 15, 23, 31],
+[34, 26, 18, 10],
+[14, 22, 30, 38],
+[27, 19, 11, 3],
+[35, 29, 23, 17],
+[6, 12, 18, 24],
+[28, 22, 16, 10],
+[13, 19, 25, 31],
+[21, 15, 9, 3],
+[20, 26, 32, 38],
+[36, 30, 24, 18],
+[5, 11, 17, 23],
+[37, 31, 25, 19],
+[4, 10, 16, 22],
+[2, 10, 18, 26],
+[39, 31, 23, 15],
+[1, 9, 17, 25],
+[40, 32, 24, 16],
+[9, 17, 25, 33],
+[8, 16, 24, 32],
+[11, 17, 23, 29],
+[12, 18, 24, 30],
+[1, 2, 3, 4],
+[5, 4, 3, 2],
+[8, 9, 10, 11],
+[12, 11, 10, 9],
+[15, 16, 17, 18],
+[19, 18, 17, 16],
+[22, 23, 24, 25],
+[26, 25, 24, 23],
+[29, 30, 31, 32],
+[33, 32, 31, 30],
+[36, 37, 38, 39],
+[40, 39, 38, 37],
+[7, 14, 21, 28],
+[8, 15, 22, 29],
+[9, 16, 23, 30],
+[10, 17, 24, 31],
+[11, 18, 25, 32],
+[12, 19, 26, 33],
+[13, 20, 27, 34]
+]
 /*-------------- Event Listeners --------------*/
 
 boardEl.addEventListener("click", handleClick)
@@ -105,7 +176,7 @@ function render() {
 
 function handleMessages() {
   if(isWinner === false){
-    gameMessage = `It is ${chipColor}'s turn'`
+    gameMessage = `It is ${chipColor}'s turn`
   } else if (isWinner === 1){
     gameMessage = "Congrats! BLACK Won!"
   } else if (isWinner === -1){
@@ -153,36 +224,12 @@ function findOpenSpace(targetColumn){
 
 function determineWinner() {
   
-  //for vertical
-  // if (Math.abs(boardState[0] + boardState[1] + boardState[2] + boardState[3] + boardState[4] + boardState[5] + boardState[6]) === 4){
-  //   isWinner = (playerTurn * -1)
-  // }  
-
-  if ( Math.abs( boardState[35] + boardState[36] + boardState[37] + boardState[38] + boardState[39] + boardState[40] + boardState[41] ) === 4 ){
-    isWinner = (playerTurn * -1)
-    console.log(isWinner)
-  }
-
-  //  if (runningTotal === 4){
-  //   winningValue = boardState[i]
-  //   console.log(winningValue)
-  //   if (winningValue === 1){
-  //     isWinner = "Black"
-  //   } else if(winningValue === -1){ 
-  //     isWinner = "Red"
-  //   }
-  //   console.log("winner:", isWinner)
-  //  }
-    render()
- 
-  // let currentVal = boardState[35]
-  // let total = currentVal
-  // for (let i = 35; i <= 41; i++){
-  //   if (boardState[i] === boardState[i-1]){
-  //     total = total + boardState[i-1]
-  //   } else if (boardState[1] !== boardState[i-1]){
-  //     total = boardState[1]
-  //   }
+  winningCombos.forEach(combo => {
+    if ( Math.abs( boardState[combo[0]] + boardState[combo[1]] + boardState[combo[2]] + boardState[combo[3]] ) === 4 ){
+      isWinner = (playerTurn * -1)
+    }
+  })
+  render()
 }
 
 // 1. A connect four board, a reset button, and the number of moves remaining for each side is visible.
