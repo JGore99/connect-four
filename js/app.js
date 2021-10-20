a8203da3e07f22fda4333d1c8fda42026db629f3
Jeff_Gore <JefGore@gmail.com>
2021-10-19 22:24:39 -0400

    broken win logic for bottom row

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ const redStatus = document.querySelector(".red-status")
 const resetBtn = document.querySelector(".reset")
 
 /*----------------- Constants -----------------*/
-const column0 = [topSquares[0], boardSquares[7], boardSquares[14], boardSquares[21], boardSquares[28], boardSquares[35]] 
-const column1 = [topSquares[1], boardSquares[8], boardSquares[15], boardSquares[22], boardSquares[29], boardSquares[36]] 
-const column2 = [topSquares[2], boardSquares[9], boardSquares[16], boardSquares[23], boardSquares[30], boardSquares[37]] 
-const column3 = [topSquares[3], boardSquares[10], boardSquares[17], boardSquares[24], boardSquares[31], boardSquares[38]] 
-const column4 = [topSquares[4], boardSquares[11], boardSquares[18], boardSquares[25], boardSquares[32], boardSquares[39]] 
-const column5 = [topSquares[5], boardSquares[12], boardSquares[19], boardSquares[26], boardSquares[33], boardSquares[40]] 
-const column6 = [topSquares[6], boardSquares[13], boardSquares[20], boardSquares[28], boardSquares[34], boardSquares[41]] 
+// const column0 = [topSquares[0], boardSquares[7], boardSquares[14], boardSquares[21], boardSquares[28], boardSquares[35]] 
+// const column1 = [topSquares[1], boardSquares[8], boardSquares[15], boardSquares[22], boardSquares[29], boardSquares[36]] 
+// const column2 = [topSquares[2], boardSquares[9], boardSquares[16], boardSquares[23], boardSquares[30], boardSquares[37]] 
+// const column3 = [topSquares[3], boardSquares[10], boardSquares[17], boardSquares[24], boardSquares[31], boardSquares[38]] 
+// const column4 = [topSquares[4], boardSquares[11], boardSquares[18], boardSquares[25], boardSquares[32], boardSquares[39]] 
+// const column5 = [topSquares[5], boardSquares[12], boardSquares[19], boardSquares[26], boardSquares[33], boardSquares[40]] 
+// const column6 = [topSquares[6], boardSquares[13], boardSquares[20], boardSquares[28], boardSquares[34], boardSquares[41]] 
 
 
 /*------------- Variables (state) -------------*/
@@ -48,26 +48,19 @@ clearAll()
 function handleClick(e) {
   
   if (e.target.classList.value !== "board") {
-    console.log(e.target.classList)
     let targetColumn = parseInt(e.target.classList[1].slice(4))
-    let targetId = parseInt(e.target.id);
+    // let targetId = parseInt(e.target.id);
     
     if(boardState[targetColumn] === null) {
       if (isWinner === false) {
-        // playerTurn === 1 ? (boardState[targetId] = -1) : (boardState[targetId] = 1);
-        console.log("playerTurn before change", playerTurn)
         findOpenSpace(targetColumn)
         playerTurn = playerTurn * -1
-        console.log("playerTurn after change", playerTurn)
         turnCounter()
         playerTurn === 1 ? chipColor = "BLACK" : chipColor = "RED"
       } else {
         return
       }
-      console.log("column", targetColumn)
-      console.log("space", targetId)
-      // console.log(boardState)
-      render(targetColumn)
+      render()
       determineWinner()
       handleMessages()
     }
@@ -113,9 +106,9 @@ function render() {
 function handleMessages() {
   if(isWinner === false){
     gameMessage = `It is ${chipColor}'s turn'`
-  } else if (isWinner === "BLACK"){
+  } else if (isWinner === 1){
     gameMessage = "Congrats! BLACK Won!"
-  } else if (isWinner === "RED"){
+  } else if (isWinner === -1){
     gameMessage = "Congrats! RED Won!"
   } else if (isWinner === "Tie"){
     gameMessage = "It's a tie! Play again?"
@@ -133,7 +126,6 @@ function turnCounter(){
 }
 
 function findOpenSpace(targetColumn){
-  console.log(targetColumn)
   let bottomColIndx = targetColumn + 35
   let bottomMinusOne = targetColumn + 28 
   let bottomMinusTwo = targetColumn + 21
@@ -143,31 +135,54 @@ function findOpenSpace(targetColumn){
 
   if (boardState[bottomColIndx] === null){
     boardState[bottomColIndx] = playerTurn
-    console.log("board state", boardState)
   } else if (boardState[bottomMinusOne] === null){
     boardState[bottomMinusOne] = playerTurn
-    console.log("board state", boardState)
   } else if (boardState[bottomMinusTwo] === null){
     boardState[bottomMinusTwo] = playerTurn
-    console.log("board state", boardState)  
   } else if (boardState[bottomMinusThree] === null){
     boardState[bottomMinusThree] = playerTurn
-    console.log("board state", boardState)   
   } else if (boardState[bottomMinusFour] === null){
     boardState[bottomMinusFour] = playerTurn
-    console.log("board state", boardState) 
   } else if (boardState[bottomMinusFive] === null){
     boardState[bottomMinusFive] = playerTurn
-    console.log("board state", boardState) 
   }
+  console.log(boardState)
   render()
 }
 
 
 function determineWinner() {
-  //for vertical 
+  
+  //for vertical
+  // if (Math.abs(boardState[0] + boardState[1] + boardState[2] + boardState[3] + boardState[4] + boardState[5] + boardState[6]) === 4){
+  //   isWinner = (playerTurn * -1)
+  // }  
 
-  // loop over each row, colum, diagonal seperately. adding the value of each chip of matching color / value. if the next chip is not of equal value or is null, the count is equal to the next chip, and contines going up until the run end, or the next chip is a different color.
+  if ( Math.abs( boardState[35] + boardState[36] + boardState[37] + boardState[38] + boardState[39] + boardState[40] + boardState[41] ) === 4 ){
+    isWinner = (playerTurn * -1)
+    console.log(isWinner)
+  }
+
+  //  if (runningTotal === 4){
+  //   winningValue = boardState[i]
+  //   console.log(winningValue)
+  //   if (winningValue === 1){
+  //     isWinner = "Black"
+  //   } else if(winningValue === -1){ 
+  //     isWinner = "Red"
+  //   }
+  //   console.log("winner:", isWinner)
+  //  }
+    render()
+ 
+  // let currentVal = boardState[35]
+  // let total = currentVal
+  // for (let i = 35; i <= 41; i++){
+  //   if (boardState[i] === boardState[i-1]){
+  //     total = total + boardState[i-1]
+  //   } else if (boardState[1] !== boardState[i-1]){
+  //     total = boardState[1]
+  //   }
 }
 
 // 1. A connect four board, a reset button, and the number of moves remaining for each side is visible.
